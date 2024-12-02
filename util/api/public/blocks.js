bbcb023def576d0073b4b8c66145473a49f6c7ba
Ronald Lonnborg Jr <kriptokyng@gmail.com>
2024-12-02 17:59:21 -0600

    new
    new

--- util/api/public/blocks.js
+++ util/api/public/blocks.js
@@ -2,7 +2,7 @@ import { mainAPIConfig } from "../../../config/api/main";
 
 export const getBlocks = async () => {
   const req = await fetch(`${mainAPIConfig.ROOT_URL}/api/blocks`);
-  return req.json();
+  return json.blocks;
 };
 
 
